6f45339c9924000f291ffb52afff1e263475d99e
Frayu <galiano@outlook.com>
2023-07-16 17:48:09 -0500

    logo local con import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import HeaderResults from "./HeaderResults"
 import Nav from "./Nav"
+import Logo from "../img/logo.png"
 
 const Header = ({handleSumit, filtrado, total}) => {
   return (
     <>
         <div id="contenedor-cabecera">
             <div id="logo">
-                <img src="https://windbnb-page.netlify.app/static/media/logo.beef9462.svg" alt="Logo Windbnb" />
+                <img src={Logo} alt="Logo Windbnb" />
             </div>
             <Nav handleSumit={handleSumit} filtrado={filtrado} />            
         </div>
